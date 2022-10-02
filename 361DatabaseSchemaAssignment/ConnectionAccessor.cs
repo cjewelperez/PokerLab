0d4df6558d3f5a291081a7c75ad9ef8dc886dce2
ppande574 <ppande0221@gmail.com>
2022-10-02 00:07:32 -0500

    Added Web.config file and added method to retrieve and store database connection string.

--- 361DatabaseSchemaAssignment/ConnectionAccessor.cs
+++ 361DatabaseSchemaAssignment/ConnectionAccessor.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Configuration; 
 namespace _361DatabaseSchemaAssignment
 {
     public class ConnectionAccessor
     {
-        public static void TestDBConnection()
+
+        public static string GetConnectionString(string keyname)
         {
-            
+            string connection = string.Empty;
+
+            if(keyname.Equals("DatabaseConnection"))
+            {
+                connection = ConfigurationManager.ConnectionStrings["DatabaseConnection"].ConnectionString;
+            }
+
+            return connection; 
         }
+
     }
 }
 
